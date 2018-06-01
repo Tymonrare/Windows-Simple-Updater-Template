e2bcd2f21d66b6f379b2f95c055ade366754e32d
Tymonr <Tymonr@yandex.ru>
2018-06-01 06:06:09 +0300

    new update mode

--- update.bat
+++ update.bat
@@ -1,10 +1,8 @@
 set download="https://drive.google.com/uc?export=download&id=//you-file-id//"
-set saveAs=bin.7z
-set sevzipPath="C:\Program Files\7-Zip\7z.exe"
+set saveAs=file
 
 cscript /nologo update.js %download% ./%saveAs%_tmp
-move /Y %saveAs%_tmp %saveAs%
 
-%sevzipPath% x %saveAs% -aoa
+move /Y %saveAs%_tmp %saveAs%
 
 del %saveAs%
